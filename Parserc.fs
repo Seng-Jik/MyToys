97ae1d91998972a3a1abe6e7aa51239f5f1e7cab
Seng Jik <853974536@qq.com>
2019-08-27 12:46:02 +0800

    Update Parserc.fs

--- Parserc.fs
+++ Parserc.fs
@@ -4,6 +4,71 @@ type str = char[]
 type parsed<'a> = ('a * str) option
 type parser<'a> = str -> 'a parsed
 
+module Parsed =
+    let map (f:'a->'b) : 'a parsed -> 'b parsed =
+        Option.map (fun (a,input) -> f a,input)
+
+    let fst<'a,'b> : ('a*'b) parsed -> 'a parsed = map fst
+    let snd<'a,'b> : ('a*'b) parsed -> 'b parsed = map snd
+
+let (<||||>) (a:'a parser) (b:'b parser) : Choice<'a,'b> parser = fun input ->
+    input
+    |> a
+    |> function
+    | Some (result,reminder) -> Some (Choice1Of2(result),reminder)
+    | None ->
+         match b input with
+         | Some (result,reminder) -> Some (Choice2Of2(result),reminder)
+         | None -> None
+         
+let (<|>) (a:'a parser) (b:'a parser) : 'a parser =
+    a <||||> b
+    >> function
+    | Some (result,reminder) ->
+        match result with
+        | Choice1Of2 x -> Some (x,reminder)
+        | Choice2Of2 x -> Some (x,reminder)
+    | None -> None
+         
+let (<+>) (a:'a parser) (b:'b parser) : ('a * 'b) parser = 
+    a
+    >> function
+    | Some (result1,reminder1) ->
+        match b reminder1 with
+        | Some (result2,reminder2) ->
+            Some ((result1,result2),reminder2)
+        | None -> None
+    | None -> None
+    
+let rec oneOrMore p = 
+    p
+    >> function
+    | Some (result,reminder) -> 
+       oneOrMore p reminder
+       |> function
+       | Some (results,finalReminder) ->
+           Some(result::results,finalReminder)
+       | None -> Some ([result],reminder)
+    | None -> None
+    
+let zeroOrMore p = fun input ->
+    input
+    |> oneOrMore p
+    |> function
+    | Some x -> Some x
+    | None -> Some ([],input)
+
+let literal (expected:string) = 
+    let rec impl expected input =
+        if Array.isEmpty expected then
+            Some ((),input)
+        else
+            match Array.tryHead input with
+            | Some x when x = Array.head expected ->
+                impl (Array.tail expected) (Array.tail input)
+            | _ -> None
+    impl (expected.ToCharArray())
+
 let characterInCondition condition = fun input ->
     match Array.tryHead input with
     | Some x when condition x -> Some (x,Array.tail input)
@@ -13,33 +78,34 @@ let characterInRange min max = characterInCondition (fun x -> x >= min && x <= m
 let character x = characterInRange x x
 let littleLetter = characterInRange 'a' 'z'
 let bigLetter = characterInRange 'A' 'Z'
+let letter = bigLetter <|> littleLetter
 let numberCharacter = characterInRange '0' '9'
 
-let (<||>) a b = 
-    a
-    >> function
-    | Some (result,reminder) -> Some (Choice1Of2(result),reminder)
-    | None ->
-         match b input with
-         | Some (result,reminder) -> Some (Choice2Of2(result),reminder)
-         | None -> None
-         
-let (<|>) a b = 
-    >> a <||> b
-    >> function
-    | Some (result,reminder) ->
-        match result with
-        | Choice1Of2 x -> x
-        | Choice2Of2 x -> x
-    | None -> None
-         
-let (<+>) a b = 
-    a
-    >> function
-    | Some (result1,reminder1) ->
-        match b reminder1 with
-        | Some (result2,reminder2) ->
-            Some (result1,result2)
-        | None -> None
-    | None -> None
+let identifier =
+    character '_' <|> letter <+> zeroOrMore (character '_' <|> letter <|> numberCharacter <|> character '-')
+    >> Parsed.map (fun (a,ls) -> a::ls |> List.toArray |> System.String)
     
+
+// test
+let parse_joe = literal "Hello Joe!"
+assert(Some((),[||]) = parse_joe ("Hello Joe!".ToCharArray()))
+assert(Some((),(" Hello Robert!".ToCharArray())) = parse_joe ("Hello Joe! Hello Robert!".ToCharArray()))
+assert(None = parse_joe ("Hello Mike!".ToCharArray()))
+
+assert(character '_' ("_up".ToCharArray()) = Some('_',"up".ToCharArray()))
+assert(letter ("abc".ToCharArray()) = Some('a',"bc".ToCharArray()))
+assert(letter ("Abc".ToCharArray()) = Some('A',"bc".ToCharArray()))
+assert((zeroOrMore letter) ("aaAA11".ToCharArray()) = Some(['a';'a';'A';'A'],"11".ToCharArray()))
+
+assert(identifier ("i-am-an-identifier".ToCharArray()) = Some("i-am-an-identifier",[||]))
+assert(identifier ("not entirely an identifier".ToCharArray()) = Some("not"," entirely an identifier".ToCharArray()))
+assert(identifier ("!not at all an identifier".ToCharArray()) = None)
+
+let parse_ha = literal "ha"
+assert(Some([();();()],[||]) = oneOrMore parse_ha ("hahaha".ToCharArray()))
+assert(None = oneOrMore parse_ha ("ahah".ToCharArray()))
+assert(None = oneOrMore parse_ha [||])
+
+assert(Some([();();()],[||]) = zeroOrMore parse_ha ("hahaha".ToCharArray()))
+assert(Some([],"ahah".ToCharArray()) = zeroOrMore parse_ha ("ahah".ToCharArray()))
+assert(Some([],[||]) = zeroOrMore parse_ha [||])
