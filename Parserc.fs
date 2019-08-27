c0d5b2b6fbed22c0a498ef072c74160e9063ce3c
Seng Jik <853974536@qq.com>
2019-08-27 09:40:33 +0800

    Update Parserc.fs

--- Parserc.fs
+++ Parserc.fs
@@ -1,12 +1,12 @@
 module Parserc
 
 type str = char[]
-type parsed = ('a * str) option
-type Parser<'a> = str -> parsed
+type parsed<'a> = ('a * str) option
+type parser<'a> = str -> 'a parsed
 
-let characterInCondition condition : Parser<char> = fun input ->
+let characterInCondition condition = fun input ->
     match Array.tryHead input with
-    | Some x when condition x -> (x,Array.tail input)
+    | Some x when condition x -> Some (x,Array.tail input)
     | _ -> None
 
 let characterInRange min max = characterInCondition (fun x -> x >= min && x <= max)
@@ -14,3 +14,32 @@ let character x = characterInRange x x
 let littleLetter = characterInRange 'a' 'z'
 let bigLetter = characterInRange 'A' 'Z'
 let numberCharacter = characterInRange '0' '9'
+
+let (<||>) a b = 
+    a
+    >> function
+    | Some (result,reminder) -> Some (Choice1Of2(result),reminder)
+    | None ->
+         match b input with
+         | Some (result,reminder) -> Some (Choice2Of2(result),reminder)
+         | None -> None
+         
+let (<|>) a b = 
+    >> a <||> b
+    >> function
+    | Some (result,reminder) ->
+        match result with
+        | Choice1Of2 x -> x
+        | Choice2Of2 x -> x
+    | None -> None
+         
+let (<+>) a b = 
+    a
+    >> function
+    | Some (result1,reminder1) ->
+        match b reminder1 with
+        | Some (result2,reminder2) ->
+            Some (result1,result2)
+        | None -> None
+    | None -> None
+    
