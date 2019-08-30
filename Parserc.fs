0e20740fffd52fecebab5877eb09a5c166078f47
Seng Jik <853974536@qq.com>
2019-08-30 15:57:47 +0800

    Update Parserc.fs

--- Parserc.fs
+++ Parserc.fs
@@ -7,6 +7,7 @@ type parser<'a> = str -> 'a parsed
 module Parsed =
     let map (f:'a->'b) : 'a parsed -> 'b parsed =
         Option.map (fun (a,input) -> f a,input)
+    let ignore = map ignore
 
     let fst<'a,'b> : ('a*'b) parsed -> 'a parsed = map fst
     let snd<'a,'b> : ('a*'b) parsed -> 'b parsed = map snd
@@ -58,6 +59,7 @@ let zeroOrMore p = fun input ->
     | Some x -> Some x
     | None -> Some ([],input)
 
+
 let literal (expected:string) = 
     let rec impl expected input =
         if Array.isEmpty expected then
@@ -69,10 +71,18 @@ let literal (expected:string) =
             | _ -> None
     impl (expected.ToCharArray())
 
-let characterInCondition condition = fun input ->
-    match Array.tryHead input with
-    | Some x when condition x -> Some (x,Array.tail input)
+
+let pred condition parser = fun input ->
+    match parser input with
+    | Some (p,remainder) when condition p -> Some (p,remainder)
     | _ -> None
+    
+let anyCharacter = fun input ->
+    match Array.tryHead input with
+    | Some x -> Some (x,Array.tail input)
+    | _ -> None
+    
+let characterInCondition condition = pred condition anyCharacter
 
 let characterInRange min max = characterInCondition (fun x -> x >= min && x <= max)
 let character x = characterInRange x x
@@ -81,31 +91,79 @@ let bigLetter = characterInRange 'A' 'Z'
 let letter = bigLetter <|> littleLetter
 let numberCharacter = characterInRange '0' '9'
 
+let whitespace = characterInCondition (fun x -> x = ' ' || x = '\t') >> Parsed.ignore
+
+
+let quotedString = 
+    character '\"' <+> zeroOrMore (characterInCondition (fun x -> x <> '\"')) <+> character '\"'
+    >> Parsed.fst >> Parsed.snd >> Parsed.map List.toArray >> Parsed.map System.String
+
+assert (quotedString ("\"Hello Joe!\"".ToCharArray()) = Some("Hello Joe!",[||]))
+
 let identifier =
     character '_' <|> letter <+> zeroOrMore (character '_' <|> letter <|> numberCharacter <|> character '-')
     >> Parsed.map (fun (a,ls) -> a::ls |> List.toArray |> System.String)
+   
+module XMLParser =
+    type XMLAttribute = {
+        key : string
+        value : string
+    }
     
+    type XMLElement = {
+        tag : string
+        attr : XMLAttribute list
+        children : XMLElement list
+    }
 
-// test
-let parse_joe = literal "Hello Joe!"
-assert(Some((),[||]) = parse_joe ("Hello Joe!".ToCharArray()))
-assert(Some((),(" Hello Robert!".ToCharArray())) = parse_joe ("Hello Joe! Hello Robert!".ToCharArray()))
-assert(None = parse_joe ("Hello Mike!".ToCharArray()))
+    let attribute = 
+        identifier <+> zeroOrMore whitespace <+> character '=' <+> zeroOrMore whitespace <+> quotedString
+        >> Parsed.map (fun x -> 
+            let value = snd x
+            let key = x |> fst |> fst |> fst |> fst
+            { key = key;value = value })
+        
+    let attributes =
+        zeroOrMore (oneOrMore whitespace <+> attribute)
+        >> Parsed.map (List.map snd)
+        
+    let elementHeadBody =
+        character '<' <+> zeroOrMore whitespace <+> identifier <+> attributes
+        >> Parsed.map (fun x -> {
+            attr = snd x
+            children = []
+            tag = x |> fst |> snd })
+    let singleElement =
+        elementHeadBody <+> zeroOrMore whitespace <+> character '/' <+> zeroOrMore whitespace <+> character '>'
+        >> Parsed.fst
+        >> Parsed.fst
+        >> Parsed.fst
+        >> Parsed.fst
+    
+    printfn "%A" (attributes (" stupid    = \"oh my gooooood!\" super=\"fff\"".ToCharArray()))
+    printfn "%A" (singleElement ("<super xx= \"abc\" yy = \"super\" />".ToCharArray()))
 
-assert(character '_' ("_up".ToCharArray()) = Some('_',"up".ToCharArray()))
-assert(letter ("abc".ToCharArray()) = Some('a',"bc".ToCharArray()))
-assert(letter ("Abc".ToCharArray()) = Some('A',"bc".ToCharArray()))
-assert((zeroOrMore letter) ("aaAA11".ToCharArray()) = Some(['a';'a';'A';'A'],"11".ToCharArray()))
 
-assert(identifier ("i-am-an-identifier".ToCharArray()) = Some("i-am-an-identifier",[||]))
-assert(identifier ("not entirely an identifier".ToCharArray()) = Some("not"," entirely an identifier".ToCharArray()))
-assert(identifier ("!not at all an identifier".ToCharArray()) = None)
+module Tests =
+    let parse_joe = literal "Hello Joe!"
+    assert(Some((),[||]) = parse_joe ("Hello Joe!".ToCharArray()))
+    assert(Some((),(" Hello Robert!".ToCharArray())) = parse_joe ("Hello Joe! Hello Robert!".ToCharArray()))
+    assert(None = parse_joe ("Hello Mike!".ToCharArray()))
 
-let parse_ha = literal "ha"
-assert(Some([();();()],[||]) = oneOrMore parse_ha ("hahaha".ToCharArray()))
-assert(None = oneOrMore parse_ha ("ahah".ToCharArray()))
-assert(None = oneOrMore parse_ha [||])
+    assert(character '_' ("_up".ToCharArray()) = Some('_',"up".ToCharArray()))
+    assert(letter ("abc".ToCharArray()) = Some('a',"bc".ToCharArray()))
+    assert(letter ("Abc".ToCharArray()) = Some('A',"bc".ToCharArray()))
+    assert((zeroOrMore letter) ("aaAA11".ToCharArray()) = Some(['a';'a';'A';'A'],"11".ToCharArray()))
 
-assert(Some([();();()],[||]) = zeroOrMore parse_ha ("hahaha".ToCharArray()))
-assert(Some([],"ahah".ToCharArray()) = zeroOrMore parse_ha ("ahah".ToCharArray()))
-assert(Some([],[||]) = zeroOrMore parse_ha [||])
+    assert(identifier ("i-am-an-identifier".ToCharArray()) = Some("i-am-an-identifier",[||]))
+    assert(identifier ("not entirely an identifier".ToCharArray()) = Some("not"," entirely an identifier".ToCharArray()))
+    assert(identifier ("!not at all an identifier".ToCharArray()) = None)
+
+    let parse_ha = literal "ha"
+    assert(Some([();();()],[||]) = oneOrMore parse_ha ("hahaha".ToCharArray()))
+    assert(None = oneOrMore parse_ha ("ahah".ToCharArray()))
+    assert(None = oneOrMore parse_ha [||])
+
+    assert(Some([();();()],[||]) = zeroOrMore parse_ha ("hahaha".ToCharArray()))
+    assert(Some([],"ahah".ToCharArray()) = zeroOrMore parse_ha ("ahah".ToCharArray()))
+    assert(Some([],[||]) = zeroOrMore parse_ha [||])
