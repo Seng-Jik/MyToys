a637862777cea165f1546c4076a13301bcda3961
Seng Jik <853974536@qq.com>
2021-04-15 23:21:20 +0800

    Update Quint.fs

--- Quint.fs
+++ Quint.fs
@@ -1,19 +1,15 @@
 let x = [
   "let x = ["
-  ""
   "]"
   ""
   "let f (x: string) = \"  \\\"\" + x.Replace(\"\\\\\",\"\\\\\\\\\").Replace(\"\\\"\", \"\\\\\\\"\") + \"\\\"\""
   ""
   "x.[0] :: List.map f x @ x.[1..]"
   "|> List.iter (printfn \"%s\")"
-
 ]
 
 let f (x: string) = "  \"" + x.Replace("\\","\\\\").Replace("\"", "\\\"") + "\""
 
 x.[0] :: List.map f x @ x.[1..]
 |> List.iter (printfn "%s")
-
-
 
